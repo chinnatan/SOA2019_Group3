5ea547fe6c0ae73e02fde0d1ab5614cdf8388c1e
KCNT <korn.chinnatan@gmail.com>
2019-03-11 15:14:43 +0700

    Init Spring Boot

---
+++ eleaving/src/main/java/it/kmitl/soa/eleaving/EleavingApplication.java
@@ -0,0 +1,13 @@
+package it.kmitl.soa.eleaving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleavingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleavingApplication.class, args);
+	}
+
+}
---
+++ eleaving/src/test/java/it/kmitl/soa/eleaving/EleavingApplicationTests.java
@@ -0,0 +1,16 @@
+package it.kmitl.soa.eleaving;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EleavingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
